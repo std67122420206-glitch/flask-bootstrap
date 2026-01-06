a40e5171b5598d95adaa8a63542181764419d2aa
std67122420206-glitch <std.67122420206@ubru.ac.th>
2026-01-06 11:21:07 +0700

    1 commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# week07
+# Chapter04
+## Flask & Bootstrap & Pythonanywhere
+*บทนี้เราจะได้เรียนรู้การใช้งานFlaskและbootstrap*
---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+cars = [
+    {'id':1, 'brand':'Toyota','model':'Yaris Ativ','year':2024, 'price':560000}
+    {'id':2, 'brand':'Toyota','model':'Yaris Cross','year':2024, 'price':760000}
+    {'id':3, 'brand':'Nissan','model':'Kicks','year':2024, 'price':860000}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html',title='Home Page')
+
+@app.route('/cars')
+def show_cars():
+    return render_template('cars/cars.html',title='Show All Cars Page', cars=cars)
+
+
+
+@app.route('/cars/new',methods=['GET','POST'])
+def new_car():
+    if request.method == 'POST':
+        brand = request.form['brand']
+        model = request.form['model']
+        year = (request.form['year'])
+        price = (request.form['price'])
+        
+        length = len(cars)
+      id cars[length-1]['id'] + 1
+      car = {'id':1, 'brand':'Toyota','model':'Yaris Ativ','year':2024, 'price':560000}
+      
+      cars.append(car)
+      return redirect(url_for('show_cars'))
+    return render_template('cars/new_car.html',title='New Car Page')
